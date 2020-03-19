fdfe29a145c5f5517e7843c6c2fb5f0de8060a87
yp <745694011@qq.com>
2020-03-19 17:15:27 +0800

    first commit

---
+++ src/test/java/com/atguigu/crm/Crm0312ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crm0312ApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
